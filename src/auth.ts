2abfed8864bc7f44a59ac735e9a0306bbdc2c3e2
meczajkowski <michal.erik.czajkowski@gmail.com>
2025-02-04 00:06:13 +0100

    feat: add auth starter

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,12 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
+import { prisma } from '../prisma/prisma';
+import authConfig from './auth.config';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
+  ...authConfig,
 });
