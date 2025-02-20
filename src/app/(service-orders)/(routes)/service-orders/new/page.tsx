f29e1879f55af1107d5fd3c06633eb19772ad7da
meczajkowski <michal.erik.czajkowski@gmail.com>
2025-02-20 12:21:25 +0100

    feat: add service orders list and new service order page

---
+++ src/app/(service-orders)/(routes)/service-orders/new/page.tsx
@@ -0,0 +1,78 @@
+import { getDeviceWithRelationsAction } from '@/app/(devices)/actions';
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { routes } from '@/routes';
+import Link from 'next/link';
+import { CreateServiceOrderForm } from '../../../(components)/(forms)/CreateServiceOrderForm';
+
+type NewServiceOrderPageProps = {
+  searchParams: {
+    deviceId?: string;
+  };
+};
+
+const NewServiceOrderPage = async ({
+  searchParams,
+}: NewServiceOrderPageProps) => {
+  if (!searchParams.deviceId) {
+    return <div>Device ID is required to create a service order</div>;
+  }
+
+  const device = await getDeviceWithRelationsAction(searchParams.deviceId);
+
+  if (!device) {
+    return <div>Device not found</div>;
+  }
+
+  return (
+    <div className="space-y-8">
+      <h1>Create Service Order</h1>
+      <div className="flex flex-col gap-4">
+        <div>
+          <div className="flex flex-col gap-2">
+            <h3>Konica Minolta Bizhub {device.model}</h3>
+            <span className="text-sm text-muted-foreground">
+              S/N: {device.serialNumber}
+            </span>
+            {device.customer ? (
+              <div className="my-8 space-y-3">
+                <div>
+                  <span className="font-bold">Customer: </span>
+                  {device.customer?.name}
+                </div>
+                <div>
+                  <span className="font-bold">Email: </span>
+                  {device.customer?.email}
+                </div>
+                <div>
+                  <span className="font-bold">Phone: </span>
+                  {device.customer?.phone}
+                </div>
+                <div>
+                  <span className="font-bold">Address: </span>
+                  {device.customer?.address}
+                </div>
+                <Link
+                  className={cn(
+                    buttonVariants({ variant: 'outline' }),
+                    'max-w-[200px]',
+                  )}
+                  href={routes.customers.view(device.customer.id)}
+                >
+                  View customer
+                </Link>
+              </div>
+            ) : (
+              <div className="my-8">No customer assigned</div>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <CreateServiceOrderForm deviceId={searchParams.deviceId} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewServiceOrderPage;
